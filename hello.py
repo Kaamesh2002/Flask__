f969bd7612e3b5fba5b3208d77af9d4b90f5bdcb
Kaamesh2002 <kaameshkumaravel2002@gmail.com>
2024-09-20 00:19:18 +0530

    commit 3

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "kaamesh2002"
+
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What is your name",validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 
@@ -17,6 +24,7 @@ def index():
     first_name = 'john'
     stuff = 'this is <strong>bold</strong>'
     pizza = ['pepper','mayo', 41]
+    flash("dummy")
     return render_template("index.html", first_name = first_name,stuff = stuff,favourite_pizza = pizza)
 
 @app.route('/user/<name>')
@@ -37,4 +45,12 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"),500
 
-
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("name.html",name = name,form= form ) 
